856236a7d3ab73ce885ccfedc0163906ce8701f3
hadadzhi <hadadzhi@gmail.com>
2016-10-07 22:24:39 +0300

    .

--- src/brave_true_clojure/infix.clj
+++ src/brave_true_clojure/infix.clj
@@ -9,6 +9,10 @@
 (defn- precedence [token]
   (operators token))
 
+(defn- bad-syntax
+  ([] (throw (IllegalArgumentException. "bad syntax")))
+  ([token] (throw (IllegalArgumentException. (str "bad syntax: " token)))))
+
 (defn- infix-to-rpn
   "Praise Dijkstra for this shunting yard algorithm"
   [tokens]
@@ -23,7 +27,7 @@
               (if (and last-op (<= (precedence last-op) (precedence token)))
                 (recur tokens (rest stack) (conj output (first stack)))
                 (recur (rest tokens) (cons token stack) output)))
-            (throw (IllegalArgumentException. (str token))))))
+            (bad-syntax token))))
       (into output stack))))
 
 (defn- rpn-to-prefix [rpn]
@@ -35,11 +39,13 @@
           (let [left (second stack),
                 right (first stack),
                 stack (rest (rest stack))]
-            (recur (rest tokens) (cons (list token left right) stack)))
-          (throw (IllegalArgumentException. (str token)))))
+            (if (and left right)
+              (recur (rest tokens) (cons (list token left right) stack))
+              (bad-syntax)))
+          (bad-syntax token)))
       (if (= 1 (count stack))
         (first stack)
-        (throw (IllegalArgumentException. "bad infix"))))))
+        (bad-syntax)))))
 
 (defmacro infix [& tokens]
   (rpn-to-prefix (infix-to-rpn tokens)))
