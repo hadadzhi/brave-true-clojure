4dd6d4d754ef261055fa26f6dfd343eb39762752
hadadzhi <hadadzhi@gmail.com>
2016-10-08 22:19:36 +0300

    .

--- test/brave_true_clojure/infix_test.clj
+++ test/brave_true_clojure/infix_test.clj
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer :all]
             [brave-true-clojure.infix :refer :all]))
 
-(deftest infix-tests
+(deftest calculations
   (testing "Operators and precedence"
     (is (= 31/5 (infix 1 + 2 * 3 - 4 / 5)))
     (is (= (+ 1 (mod  2 -3)) (infix 1 + 2 mod -3)))
@@ -11,7 +11,18 @@
   (testing "Grouping"
     (is (= -3/5 (infix [1 + 2] * [3 - 4] / 5))))
   (testing "Function calls"
-    (is (= (Math/sin (/ Math/PI 2)) (infix (Math/sin (infix Math/PI / 2))))))
+    (is (= (Math/sin (/ Math/PI 2))
+           (infix (Math/sin (infix Math/PI / 2))))))
   (testing "Symbols"
     (let [x 2, y 3]
       (is (= (* x y) (infix x * y))))))
+
+(deftest bad-syntax
+  (testing "Bad syntax"
+    (let [valid-infix? #'brave-true-clojure.infix/valid-infix?]
+      (is (not (valid-infix? [])))
+      (is (not (valid-infix? [[]])))
+      (is (not (valid-infix? [[] + 1])))
+      (is (not (valid-infix? [1 - 2 +])))
+      (is (not (valid-infix? [+ 1 - 2])))
+      (is (not (valid-infix? [1 2]))))))
