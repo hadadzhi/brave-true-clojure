dd44f70f53373a4ef8860776d64aeed40e1632a4
hadadzhi <hadadzhi@gmail.com>
2017-05-21 22:17:59 +0300

    .

--- src/brave_true_clojure/wrand.clj
+++ src/brave_true_clojure/wrand.clj
@@ -4,4 +4,6 @@
   (let [total (reduce + slices), r (rand total)]
     (loop [i 0, sum 0]
       (let [sum (+ (slices i) sum)]
-        (if (< r sum) i (recur (inc i) sum))))))
+        (if (< r sum)
+          i
+          (recur (inc i) sum))))))
