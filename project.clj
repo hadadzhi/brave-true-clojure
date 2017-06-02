1dcce03a472579d36c4ef1a54ddd0bc3caf8cbe1
hadadzhi <hadadzhi@gmail.com>
2017-06-03 00:50:15 +0300

    .

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject brave-true-clojure "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/math.combinatorics "0.1.4"]
                  [criterium "0.4.4"]]
