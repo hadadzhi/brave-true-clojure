7266514b02097006ad4169eeaf3160d568a428b7
hadadzhi <hadadzhi@gmail.com>
2017-06-11 23:24:44 +0300

    .

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject brave-true-clojure "0.1.0-SNAPSHOT"
+  :plugins [[lein-ubersource "0.1.1"]]
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/math.combinatorics "0.1.4"]
